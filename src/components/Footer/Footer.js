e69fac9d9c5d0023b37ef50d25ffacdbc76e322c
xanderflores <alexanderflores_10_@hotmail.es>
2024-09-25 14:34:31 -0300

    Agregamos el footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer(){
+    return(
+        <footer class="footer">
+            <div>Copyright (c) 2024. CILSA BOOTCAMP. Todos los derechos reservados.GRUPO 10
+            </div>
+        </footer>
+    );
+}
+export default Footer;
---
+++ src/components/Footer/dist/Footer.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+
+require("./Footer.scss");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+fun;
