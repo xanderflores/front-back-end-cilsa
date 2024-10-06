92e937dd1280fc232d8b154f47010391ed83ea8a
xanderflores <alexanderflores_10_@hotmail.es>
2024-10-06 16:53:56 -0300

    agregamos contenido a cursos, desplegando unas cards de DataCursos.js.

---
+++ src/Data/dist/DataCursos.dev.js
@@ -0,0 +1,59 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var cursos = [{
+  id: 1,
+  titulo: "Python",
+  descripcion: "Aprende Python desde cero.",
+  icono: "python"
+}, {
+  id: 2,
+  titulo: "JavaScript",
+  descripcion: "Domina JavaScript y el desarrollo web.",
+  icono: "js"
+}, {
+  id: 3,
+  titulo: "React",
+  descripcion: "Crea aplicaciones web con React.",
+  icono: "react"
+}, {
+  id: 4,
+  titulo: "Java",
+  descripcion: "Curso completo de Java.",
+  icono: "java" // Icono de Java
+
+}, {
+  id: 5,
+  titulo: "C++",
+  descripcion: "Aprende de este Framework Vue.",
+  icono: "vuejs" // Usando 'cpp' como representación, aunque no hay ícono específico en FontAwesome
+
+}, {
+  id: 6,
+  titulo: "Angular",
+  descripcion: "Desarrolla web con Angular.",
+  icono: "angular" // Usando 'gem' para representar Ruby
+
+}, {
+  id: 7,
+  titulo: "PHP",
+  descripcion: "Desarrollo web con PHP.",
+  icono: "php"
+}, {
+  id: 8,
+  titulo: "C#",
+  descripcion: "Curso de C# para aplicaciones de escritorio y web.",
+  icono: "microsoft" // No hay icono directo, pero se puede usar Microsoft para representar C#
+
+}, {
+  id: 9,
+  titulo: "Go",
+  descripcion: "Programación concurrente con Go.",
+  icono: "golang" // Usando un nombre representativo
+
+}];
+var _default = cursos;
+exports["default"] = _default;
